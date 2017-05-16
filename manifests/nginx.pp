071f4f9dbb4679cf295710d12b33902753e8d987
root <root@puppet-master.muro.local>
2017-05-16 11:54:31 +0200

    use hieradata

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,6 +1,11 @@
-class profile::nginx() {
+class profile::nginx(
+  Array $vhosts,
+) {
   include ::nginx
-
-  ::nginx::vhost {$facts['fqdn']:
+  $vhosts.each |$vhost| {
+    ::nginx::vhost {$vhost['fqdn']:
+      port           => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
+    }
   }
 }
